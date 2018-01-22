9e533aa7e494bfa5a6fd7cce63adf0f37ecf2b12
Arthur Borikun <arturborikun2@gmail.com>
2018-01-22 17:28:53 +0200

    Added basic files for React app

---
+++ src/components/App/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+        <p>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

ec6e95cb0561a3ee1acb894004ad6c47ddd0932a
Arthur Borikun <31827227+4rtHurB@users.noreply.github.com>
2018-01-25 15:37:38 +0200

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,5 @@
-export const EXTRACTOR_API = 'http://localhost:3001/api';//'http://192.168.128.232:8080/';
-export const PLATFORM_API = 'http://localhost:8000';
+export const EXTRACTOR_API = 'http://192.168.128.232:8080';
+export const PLATFORM_API = 'http://192.168.128.232:8082';
 
 export const RUN_CRAWLER_API = '/extractor/rest/v1/crawler/run';
 export const CRAWLER_INFO_API = '/extractor/rest/v1/crawler/';

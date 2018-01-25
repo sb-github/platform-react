2b43cdc8769100c0e156942707b8a93280e0653a
Arthur Borikun <arturborikun2@gmail.com>
2018-01-25 23:00:06 +0200

    fix some errors

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import DevTools from '../components/DevTools/DevTools';
-import crawlers from '../components/Crawler/crawlerReducer';
+import crawlersInfo from '../components/Crawler/crawlerReducer';
 import tree from '../components/WordTree/treeReducer';
 import {fetchCrawlers} from "../components/Crawler/crawlerActions";
 
 export const reducer = combineReducers({
-  crawlers,
+  crawlersInfo,
   tree
 });
 
@@ -18,6 +18,6 @@ const store = createStore(
   )
 );
 
-store.dispatch(fetchCrawlers());
+store.dispatch(fetchCrawlers(1));
 
 export default store;

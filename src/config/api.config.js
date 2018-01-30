8cf441c047851348ffa924cba63d341ab9fe8117
Arthur Borikun <arturborikun2@gmail.com>
2018-01-31 00:49:44 +0200

    create filtring for result of crawlers

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,10 +1,10 @@
-export const EXTRACTOR_API = 'http://192.168.128.245:8081'; //http://localhost:3001/api
-export const PLATFORM_API = 'http://192.168.128.245:8082'; //http://localhost:8000
+export const EXTRACTOR_API = 'http://localhost:3001/api'; //http://localhost:3001/api
+export const PLATFORM_API = 'http://localhost:8000'; //http://localhost:8000
 
 export const RUN_CRAWLER_API = '/extractor/rest/v1/crawler/run';
 export const CRAWLER_INFO_API = '/extractor/rest/v1/crawler/';
 export const STOP_WORDS_API = '/extractor/rest/v1/stopwords';
-export const GRAPH_SKILL_API = '/extractor/graphskill';
+export const GRAPH_SKILL_API = '/api/v1/graphskill';
 export const SKILLS_API = '/api/v1/skills';
 
 export const ADD_SKILL_API = '/api/v1/skill';

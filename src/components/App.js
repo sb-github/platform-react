d44e8c6dcdd7be8432e8f117831270192856e87f
Arthur Borikun <arturborikun2@gmail.com>
2018-02-01 00:17:49 +0200

    upgrade admin panel

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from '../store/index';
-import Page from "./AdminMenu/Page";
-
+import Root from "./Root";
 
 class App extends Component {
   render() {
     return(
       <Provider store = { store }>
-        <Page/>
+        <Root/>
       </Provider>
     );
   };
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 import DevTools from '../components/DevTools/DevTools';
-/*import CrawlerContainer from './Crawler/';
-import WordTreeContainer from "./WordTree/";
-import SKillContainer from './Skill/';
-import MaterialContainer from './Material/';
-import { BrowserRouter as Router, Route } from 'react-router-dom';*/
-import MenuContainer from './AdminMenu/';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AdminPanel from './AdminPanel/AdminPanel';
 
 class Root extends Component {
   render() {
     return(
-    	<div>
-    	  <MenuContainer />
+      <Router>
+        <div>
           <DevTools />
-          /*<Router>
-            <Route path="/crawlers" component = { CrawlerContainer } />
-          </Router>
-          <Router>
-            <Route path="/skills" component = { SKillContainer } />
-          </Router>
-          <Router>
-            <Route path="/materials" component = { MaterialContainer } />
-          </Router>*/
+          <AdminPanel/>
         </div>
+      </Router>
     );
   };
 }

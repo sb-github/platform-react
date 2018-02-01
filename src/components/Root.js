6b33d5af5380ba9159408ed6bd0959719633190c
yuzvyshyn <fakcelo@gmail.com>
2018-02-01 18:09:43 +0200

    Update Root.js

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import DevTools from '../components/DevTools/DevTools';
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom';
 import AdminPanel from './AdminPanel/AdminPanel';
 
 class Root extends Component {

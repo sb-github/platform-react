b496147f6970ff16770acbd22dd925491d0f35f3
Arthur Borikun <arturborikun2@gmail.com>
2018-02-08 23:41:19 +0200

    remove Root component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import DevTools from '../components/DevTools/DevTools';
+import AdminPanel from './AdminPanel/AdminPanel';
 import store from '../store/index';
-import Root from "./Root";
+import 'antd/dist/antd.css';
+import '@blueprintjs/core/dist/blueprint.css';
+import 'normalize.css/normalize.css';
 
 class App extends Component {
   render() {
+    const devTools = process.env.NODE_ENV !== 'production' ? <DevTools /> : <span />;
+
     return(
-      <Provider store = { store }>
-        <Root/>
-      </Provider>
+      <Router>
+        <Provider store = { store }>
+          <div>
+            {devTools}
+            <AdminPanel/>
+          </div>
+        </Provider>
+      </Router>
     );
   };
 }
